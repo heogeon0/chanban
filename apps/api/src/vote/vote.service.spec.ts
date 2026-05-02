511bc9507a38f570a1e8659b0da3e154fb7e195d
허건녕(HeoGeonNyoung) <97589715+heogeon0@users.noreply.github.com>
2026-05-02 18:08:16 +0900

    test(api): post 모듈 spec DI 미스 수정 (#22)

--- apps/api/src/vote/vote.service.spec.ts
+++ apps/api/src/vote/vote.service.spec.ts
@@ -1,4 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Post } from '../entities/post.entity';
+import { VoteHistory } from '../entities/vote-history.entity';
+import { Vote } from '../entities/vote.entity';
 import { VoteService } from './vote.service';
 
 describe('VoteService', () => {
@@ -6,7 +11,13 @@ describe('VoteService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VoteService],
+      providers: [
+        VoteService,
+        { provide: getRepositoryToken(Vote), useValue: {} },
+        { provide: getRepositoryToken(VoteHistory), useValue: {} },
+        { provide: getRepositoryToken(Post), useValue: {} },
+        { provide: DataSource, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<VoteService>(VoteService);
